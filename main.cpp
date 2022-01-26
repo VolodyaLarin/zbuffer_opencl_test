58d0e4c3687a63d027c9ccd11b6b6d1e0b261e4c
Volodya Larin <volodyalarin@bk.ru>
2022-01-26 05:26:50 +0300

    Update

--- main.cpp
+++ main.cpp
@@ -8,79 +8,263 @@
 #include <sstream>
 
 #include "glm/matrix.hpp"
+#include "imgui/imgui.h"
+#include "imgui/backends/imgui_impl_glut.h"
+#include "imgui/backends/imgui_impl_opengl3.h"
 #include <glm/gtc/matrix_transform.hpp>
 
-size_t w = 800, h = 600;
+size_t w = 1800, h = 1000;
+
+float speed_k = 1;
+
 cl::Kernel kernel;
+cl::Device device;
+cl::Context ctx;
+cl::Program program;
+
 cl_uchar3 *colorBuffer = nullptr;
+cl_float2 *velocityBuffer = nullptr;
+cl_float *depthBuffer = nullptr;
 
-void displayMe(void) {
-//    for (int i = 0; i < w * h; ++i) {
-//        colorBuffer[i].w = 255;
-//
-//    }
-//    int x = rand()% (h - 2) * w;
-//    for (int i = x; i < w  * 1 + x; ++i) {
-//        colorBuffer[i].x += 5;
-//        colorBuffer[i].y += 50;
-//    }
-    glClear(GL_COLOR_BUFFER_BIT);
-    if (!colorBuffer) {
 
-        glBegin(GL_POLYGON);
-        glVertex3f(0.0, 0.0, 0.0);
-        glVertex3f(0.5, 0.0, 0.0);
-        glVertex3f(0.5, 0.5, 0.0);
-        glVertex3f(0.0, 0.5, 0.0);
-        glEnd();
-    } else {
-        glDrawPixels(w, h, GL_RGBA, GL_UNSIGNED_BYTE, colorBuffer);
-    }
-    glFlush();
-}
+cl::Buffer cl_image;
+cl::Buffer cl_depth;
+cl::Buffer cl_velocity;
+cl_uint2 wh = {static_cast<cl_uint>(w), static_cast<cl_uint>(h)};
+cl::Buffer cl_wh;
+
+
+struct TransformationParams {
+    glm::vec3 rotate;
+    glm::vec3 scale;
+    glm::vec3 translate;
+};
+
+TransformationParams trans_camera_old = {glm::vec3(0,0,0), glm::vec3(1,1,1), glm::vec3(0,0,0)};
+TransformationParams trans_camera = {glm::vec3(0,0,0), glm::vec3(1,1,1), glm::vec3(0,0,0)};
+
+TransformationParams trans_obj_old = {glm::vec3(0,0,0), glm::vec3(1,1,1), glm::vec3(0,0,0)};
+TransformationParams trans_obj = {glm::vec3(0,0,0), glm::vec3(1,1,1), glm::vec3(0,100,0)};
+
+
+std::vector<Model> models = {};
+
+
+int mode = 1;
 
 
 void timeFunc(void) {
-//    glutPostRedisplay();
+    glutPostRedisplay();
 }
 
 typedef struct __attribute__ ((packed)) {
     cl_uchar4 color;
     cl_float4 points[3];
     cl_float16 mvp;
+    cl_float16 mvp_old;
 } cl_polygon;
 
 
+
+glm::mat4 genetate_srt(glm::mat4 M, glm::vec3 rot, glm::vec3 scale, glm::vec3 transl) {
+    M = glm::scale(M, scale);
+    M = glm::rotate(M, rot.x, glm::vec3(1.f, 0, 0));
+    M = glm::rotate(M, rot.y, glm::vec3(0, 1, 0));
+    M = glm::rotate(M, rot.z, glm::vec3(0, 0, 1));
+    M = glm::translate(M, transl);
+    return M;
+}
+
+glm::mat4 generate_mvp(glm::vec3 rot_cam, glm::vec3 scale_cam, glm::vec3 transl_cam, glm::vec3 rot, glm::vec3 scale, glm::vec3 transl){
+    glm::mat4 P = glm::mat4 (1);
+    P[2][3] = 0.001;
+
+    glm::mat4 V = genetate_srt(glm::mat4(1.f), rot_cam, scale_cam, transl_cam);
+    glm::mat4 M = genetate_srt(glm::mat4(1.f), rot, scale, transl);
+    glm::mat4 MVP = P * V * M;
+
+    return MVP;
+}
+
+
+
+void displayMe(void) {
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGLUT_NewFrame();
+
+    glClear(GL_COLOR_BUFFER_BIT);
+
+
+    std::vector<cl_polygon> polygons = {};
+    for (auto &model: models) {
+        for (auto &polygon: model.triangles) {
+            float k = 100;
+            float c = 0;
+            auto color = glm::vec3(1, 0, 0);
+            cl_polygon polygon1 = {
+                    {cl_uchar(color.x * 255), cl_uchar(color.y * 255), cl_uchar(color.z * 255), 255},
+                    {
+                     {polygon.t[0].x * k + c, polygon.t[0].y * k + c, polygon.t[0].z * k + c, 1},
+                                              {polygon.t[1].x * k + c, polygon.t[1].y * k + c, polygon.t[1].z * k + c, 1},
+                                                                       {polygon.t[2].x * k + c, polygon.t[2].y * k + c,polygon.t[2].z * k + c, 1},
+                    }
+
+            };
+
+
+            glm::mat4 MVP = generate_mvp(
+                    trans_camera.rotate, trans_camera.scale, trans_camera.translate,
+                    trans_obj.rotate, trans_obj.scale, trans_obj.translate
+            );
+            glm::mat4 MVP_old = generate_mvp(
+                    trans_camera_old.rotate, trans_camera_old.scale, trans_camera_old.translate,
+                    trans_obj_old.rotate, trans_obj_old.scale, trans_obj_old.translate
+            );
+
+            polygon1.mvp = *(cl_float16 *)(&MVP[0][0]);
+            polygon1.mvp_old = *(cl_float16 *)(&MVP_old[0][0]);
+
+            polygons.emplace_back(polygon1);
+        }
+    }
+
+    auto *cl_polygons_buffer = polygons.data();
+    auto cl_models_count = polygons.size();
+
+    cl::Buffer cl_models(
+            ctx,
+            CL_MEM_READ_ONLY | CL_MEM_ALLOC_HOST_PTR | CL_MEM_COPY_HOST_PTR,
+            sizeof(cl_polygon) * cl_models_count,
+            cl_polygons_buffer
+    );
+
+    kernel.setArg(0, cl_image);
+    kernel.setArg(1, cl_depth);
+    kernel.setArg(2, cl_wh);
+    kernel.setArg(3, cl_models);
+    kernel.setArg(4, cl_velocity);
+
+
+    cl::CommandQueue queue(ctx, device);
+    int err_q = 0;
+    err_q = queue.enqueueFillBuffer<cl_uchar4>(cl_image, {255, 255, 255, 255}, 0, w * h * sizeof (cl_uchar4));
+    err_q = queue.enqueueFillBuffer<cl_float2>(cl_velocity, {0,0}, 0, w * h * sizeof (cl_float2));
+    err_q = queue.enqueueFillBuffer<cl_float>(cl_depth, -CL_INFINITY, 0, w * h * sizeof (cl_float));
+    err_q = queue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(cl_models_count));
+
+
+    err_q = queue.enqueueReadBuffer(cl_image, CL_TRUE, 0, w * h * sizeof(cl_uchar3), colorBuffer);
+    err_q = queue.enqueueReadBuffer(cl_depth, CL_TRUE, 0, w * h * sizeof(cl_float), depthBuffer);
+    err_q = queue.enqueueReadBuffer(cl_velocity, CL_TRUE, 0, w * h * sizeof(cl_float2), velocityBuffer);
+
+    if (!mode) {
+        for (int x = 0; x < w; x++) {
+            for (int y = 0; y < h; y++) {
+                int inx = x + y * w;
+                colorBuffer[inx].x = velocityBuffer[inx].x * speed_k;
+                colorBuffer[inx].y = velocityBuffer[inx].y * speed_k;
+                colorBuffer[inx].z = 0;
+                colorBuffer[inx].w = 255;
+            }
+        }
+    }
+
+    glDrawPixels(w, h, GL_RGBA, GL_UNSIGNED_BYTE, colorBuffer);
+    glFlush();
+
+    ImGui::ShowDemoWindow();
+
+    {
+        static float f = 0.0f;
+        static int counter = 0;
+
+        ImGui::Begin("!");                          // Create a window called "Hello, world!" and append into it.
+
+        ImGui::InputInt("Mode", &mode);
+        ImGui::InputFloat3("Camera translate", &trans_camera.translate[0]);
+        ImGui::InputFloat3("Camera rotate", &trans_camera.rotate[0]);
+//        ImGui::InputFloat3("Camera scale", &trans_camera.scale[0]);
+
+        ImGui::InputFloat3("Object translate", &trans_obj.translate[0]);
+        ImGui::InputFloat3("Object rotate", &trans_obj.rotate[0]);
+        ImGui::InputFloat3("Object scale", &trans_obj.scale[0]);
+
+        ImGui::InputFloat3("Camera old translate", &trans_camera_old.translate[0]);
+        ImGui::InputFloat3("Camera old rotate", &trans_camera_old.rotate[0]);
+
+        ImGui::InputFloat3("Object old translate", &trans_obj_old.translate[0]);
+        ImGui::InputFloat3("Object old rotate", &trans_obj_old.rotate[0]);
+        ImGui::InputFloat3("Object old scale", &trans_obj_old.scale[0]);
+        ImGui::SliderFloat("speed_k", &speed_k, 0, 10);
+
+        ImGui::End();
+    }
+
+    ImGui::Render();
+
+    ImGuiIO &io = ImGui::GetIO();
+    glViewport(0, 0, (GLsizei) io.DisplaySize.x, (GLsizei) io.DisplaySize.y);
+
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+    ImGui::EndFrame();
+
+    glutSwapBuffers();
+    glutPostRedisplay();
+
+}
+
+
+
+static void initGui() {
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO &io = ImGui::GetIO();
+    (void) io;
+
+    ImFontConfig font_config;
+    font_config.OversampleH = 1; //or 2 is the same
+    font_config.OversampleV = 1;
+    font_config.PixelSnapH = 1;
+
+    static const ImWchar ranges[] =
+            {
+                    0x0020, 0x00FF, // Basic Latin + Latin Supplement
+                    0x0400, 0x044F, // Cyrillic
+                    0,
+            };
+    io.Fonts->AddFontFromFileTTF("../fonts/Tahoma.ttf", 16.0f, &font_config, ranges);
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    auto &style = ImGui::GetStyle();
+    style.FrameRounding = 10;
+    style.WindowBorderSize = 0;
+    style.FrameBorderSize = 1;
+    ImGui::StyleColorsDark(&style);
+
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplGLUT_Init();
+
+
+    ImGui_ImplGLUT_InstallFuncs();
+}
 
 int main(int argc, char *argv[]) {
     srand(time(NULL));
 
     ObjModelLoader loader;
 
-    std::vector<Model> models = {};
 
 
-    auto m = loader.load("../model.obj");
+    auto m = loader.load("../ozyx.obj");
+    models.push_back(m);
+    models.push_back(m);
+    models.push_back(m);
+    models.push_back(m);
+    models.push_back(m);
+    models.push_back(m);
     models.push_back(m);
 
-//    std::stringstream ss(
-//            "v 1.000000 1.000000 -1.000000\n"
-//            "v 1.000000 -1.000000 -1.000000\n"
-//            "v 1.000000 1.000000 1.000000\n"
-//            "v 1.000000 -1.000000 1.000000\n"
-//            "v -1.000000 1.000000 -1.000000\n"
-//            "v -1.000000 -1.000000 -1.000000\n"
-//            "v -1.000000 1.000000 1.000000\n"
-//            "v -1.000000 -1.000000 1.000000\n"
-//            "f 1/1/1 5/2/1 7/3/1 3/4/1\n"
-//            "f 4/5/2 3/4/2 7/6/2 8/7/2\n"
-//            "f 8/8/3 7/9/3 5/10/3 6/11/3\n"
-//            "f 6/12/4 2/13/4 4/5/4 8/14/4\n"
-//            "f 2/13/5 1/1/5 3/4/5 4/5/5\n"
-//            "f 6/11/6 5/10/6 1/1/6 2/13/6\n"
-//    );
-//
-//    models.push_back(loader.loadStream(ss));
 
 
     std::vector<cl::Platform> platforms;
@@ -98,7 +282,7 @@ int main(int argc, char *argv[]) {
 
     if (devices.empty()) std::cerr << "No devices found!" << std::endl;
 
-    auto device = devices.front();
+    device = devices.front();
 
 
     std::ifstream kernel_file("../test.cl");
@@ -106,8 +290,8 @@ int main(int argc, char *argv[]) {
 
     cl::Program::Sources sources = {src};
 
-    auto ctx = cl::Context(device);
-    auto program = cl::Program(ctx, sources);
+    ctx = cl::Context(device);
+    program = cl::Program(ctx, sources);
 
     auto err = program.build();
     if (err != CL_BUILD_SUCCESS) {
@@ -117,112 +301,53 @@ int main(int argc, char *argv[]) {
     }
     std::cerr << "Program was built!" << std::endl;
 
-
-    std::vector<cl_polygon> polygons = {};
-    for (auto &model: models) {
-        for (auto &polygon: model.triangles) {
-            float k = 100;
-            float c = 0;
-            auto color = glm::vec3(1, 0, 0);
-            cl_polygon polygon1 = {
-                    {cl_uchar(color.x * 255), cl_uchar(color.y * 255), cl_uchar(color.z * 255), 255},
-                    {
-                     {polygon.t[0].x * k + c, polygon.t[0].y * k + c, polygon.t[0].z * k + c, 1},
-                     {polygon.t[1].x * k + c, polygon.t[1].y * k + c, polygon.t[1].z * k + c, 1},
-                     {polygon.t[2].x * k + c, polygon.t[2].y * k + c,polygon.t[2].z * k + c, 1},
-                    }
-
-            };
-
-
-            glm::mat4 P = glm::mat4 (1);
-            P[2][3] = 0.001;
-
-//            P = glm::translate(glm::mat4(1.f), glm::vec3(float(w/2), float(h/2), 0.f))
-//                    * P *
-//                    glm::translate(glm::mat4(1.f), glm::vec3(-float(w/2), -float(h/2), 0.f)) ;
-
-//            glm::mat4 P = glm::infinitePerspective(45.f, 1.f, 0.f);
-//            glm::mat4 P = glm::perspective(90.0f, 1.f, 0.1f, -100.0f);
-
-            //VIEW
-            glm::mat4 V = glm::mat4(1.);
-            V = glm::translate(V, glm::vec3(0.0f, 0.0f, 0));
-
-            glm::mat4 M = glm::mat4(1.0);
-            M = glm::translate(M, glm::vec3(0.0f,00.0f, 0));
-            M = glm::scale(M, glm::vec3(1.3f));
-            M = glm::rotate(M, 1.1f, glm::vec3(1.f, 0, 0));
-
-            glm::mat4 MVP = P * V * M;
-
-//            glm::mat4 MVP = glm::scale(glm::mat4(1.0f),glm::vec3(0.5f));
-
-            polygon1.mvp = *(cl_float16 *)(&MVP[0][0]);
-
-            polygons.emplace_back(polygon1);
-        }
-    }
-
-    auto *cl_polygons_buffer = new cl_polygon [polygons.size()];
-    unsigned cl_models_count = 0;
-    for (auto &p: polygons) {
-        cl_polygons_buffer[cl_models_count] = p;
-        cl_models_count++;
-    }
-
     kernel = cl::Kernel(program, "render", nullptr);
 
-    cl::Buffer cl_image(
+
+    colorBuffer = new cl_uchar3[w * h];
+    depthBuffer = new cl_float[w * h];
+    velocityBuffer = new cl_float2[w * h];
+
+
+    cl_image = cl::Buffer(
             ctx,
             CL_MEM_WRITE_ONLY | CL_MEM_HOST_READ_ONLY,
             sizeof(cl_uchar3) * w * h
     );
 
-    cl::Buffer cl_depth(
+    cl_depth = cl::Buffer(
             ctx,
             CL_MEM_READ_WRITE | CL_MEM_HOST_READ_ONLY,
             sizeof(cl_float) * w * h
     );
 
-    cl::Buffer cl_models(
+    cl_velocity = cl::Buffer(
             ctx,
-            CL_MEM_READ_ONLY | CL_MEM_ALLOC_HOST_PTR | CL_MEM_COPY_HOST_PTR,
-            sizeof(cl_polygon) * cl_models_count,
-            cl_polygons_buffer
+            CL_MEM_READ_WRITE | CL_MEM_HOST_READ_ONLY,
+            sizeof(cl_float2) * w * h
     );
 
-    cl_uint2 wh = {static_cast<cl_uint>(w), static_cast<cl_uint>(h)};
-    cl::Buffer cl_wh(ctx, CL_MEM_READ_ONLY | CL_MEM_HOST_NO_ACCESS | CL_MEM_USE_HOST_PTR, sizeof(cl_uint2), &wh);
-
-
-    kernel.setArg(0, cl_image);
-    kernel.setArg(1, cl_depth);
-    kernel.setArg(2, cl_wh);
-    kernel.setArg(3, cl_models);
-
-
-    cl::CommandQueue queue(ctx, device);
-    auto err_q = queue.enqueueFillBuffer<cl_uchar4>(cl_image, {255, 255, 255, 255}, 0, w * h * sizeof (cl_uchar4));
-    err_q = queue.enqueueFillBuffer<cl_float>(cl_depth, -CL_INFINITY, 0, w * h * sizeof (cl_float));
-    err_q = queue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(cl_models_count));
-
-
-    colorBuffer = new cl_uchar3[w * h];
-    auto depthBuffer = new cl_float[w * h];
-
-    err_q = queue.enqueueReadBuffer(cl_image, CL_TRUE, 0, w * h * sizeof(cl_uchar3), colorBuffer);
-    err_q = queue.enqueueReadBuffer(cl_depth, CL_TRUE, 0, w * h * sizeof(cl_float), depthBuffer);
-
+    cl_wh = cl::Buffer(ctx, CL_MEM_READ_ONLY | CL_MEM_HOST_NO_ACCESS | CL_MEM_USE_HOST_PTR, sizeof(cl_uint2), &wh);
 
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE);
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_MULTISAMPLE);
+
     glutInitWindowSize(w, h);
     glutInitWindowPosition(100, 100);
     glutCreateWindow("SAMPLE TEST");
     glutDisplayFunc(displayMe);
     glutIdleFunc(timeFunc);
+
+    initGui();
+
+    ImGui_ImplOpenGL3_Init();
+
     glutMainLoop();
+
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGLUT_Shutdown();
+    ImGui::DestroyContext();
     return 0;
 }
 
--- test.cl
+++ test.cl
@@ -6,12 +6,197 @@ typedef struct __attribute__ ((packed)) {
     uchar4 color;
     float4 points[3];
     float16 mvp;
+    float16 mvp_old;
 } cl_polygon;
 
-float3 __cross__(float3 v1, float3 v2) {
 
-    return (float3)(v1.y*v2.z - v1.z*v2.y, v1.z*v2.x - v1.x*v2.z, v1.x*v2.y - v1.y*v2.x);
+float16 inverce(float16 m) {
+    float SubFactor00 = m[10] * m[15] - m[14] * m[11];
+	float SubFactor01 = m[9] * m[15] - m[13] * m[11];
+	float SubFactor02 = m[9] * m[14] - m[13] * m[10];
+	float SubFactor03 = m[8] * m[15] - m[12] * m[11];
+	float SubFactor04 = m[8] * m[14] - m[12] * m[10];
+	float SubFactor05 = m[8] * m[13] - m[12] * m[9];
+	float SubFactor06 = m[6] * m[15] - m[14] * m[7];
+	float SubFactor07 = m[5] * m[15] - m[13] * m[7];
+	float SubFactor08 = m[5] * m[14] - m[13] * m[6];
+	float SubFactor09 = m[4] * m[15] - m[12] * m[7];
+	float SubFactor10 = m[4] * m[14] - m[12] * m[6];
+	float SubFactor11 = m[4] * m[13] - m[12] * m[5];
+	float SubFactor12 = m[6] * m[11] - m[10] * m[7];
+	float SubFactor13 = m[5] * m[11] - m[9] * m[7];
+	float SubFactor14 = m[5] * m[10] - m[9] * m[6];
+	float SubFactor15 = m[4] * m[11] - m[8] * m[7];
+	float SubFactor16 = m[4] * m[10] - m[8] * m[6];
+	float SubFactor17 = m[4] * m[9] - m[8] * m[5];
+
+    float16 Inverse;
+    Inverse[0] = + (m[5] * SubFactor00 - m[6] * SubFactor01 + m[7] * SubFactor02);
+    Inverse[1] = - (m[4] * SubFactor00 - m[6] * SubFactor03 + m[7] * SubFactor04);
+    Inverse[2] = + (m[4] * SubFactor01 - m[5] * SubFactor03 + m[7] * SubFactor05);
+    Inverse[3] = - (m[4] * SubFactor02 - m[5] * SubFactor04 + m[6] * SubFactor05);
+
+    Inverse[4] = - (m[1] * SubFactor00 - m[2] * SubFactor01 + m[3] * SubFactor02);
+    Inverse[5] = + (m[0] * SubFactor00 - m[2] * SubFactor03 + m[3] * SubFactor04);
+    Inverse[6] = - (m[0] * SubFactor01 - m[1] * SubFactor03 + m[3] * SubFactor05);
+    Inverse[7] = + (m[0] * SubFactor02 - m[1] * SubFactor04 + m[2] * SubFactor05);
+
+    Inverse[8] = + (m[1] * SubFactor06 - m[2] * SubFactor07 + m[3] * SubFactor08);
+    Inverse[9] = - (m[0] * SubFactor06 - m[2] * SubFactor09 + m[3] * SubFactor10);
+    Inverse[10] = + (m[0] * SubFactor07 - m[1] * SubFactor09 + m[3] * SubFactor11);
+    Inverse[11] = - (m[0] * SubFactor08 - m[1] * SubFactor10 + m[2] * SubFactor11);
+
+    Inverse[12] = - (m[1] * SubFactor12 - m[2] * SubFactor13 + m[3] * SubFactor14);
+    Inverse[13] = + (m[0] * SubFactor12 - m[2] * SubFactor15 + m[3] * SubFactor16);
+    Inverse[14] = - (m[0] * SubFactor13 - m[1] * SubFactor15 + m[3] * SubFactor17);
+    Inverse[15] = + (m[0] * SubFactor14 - m[1] * SubFactor16 + m[2] * SubFactor17);
+
+    float Determinant =
+        + m[0] * Inverse[0]
+        + m[1] * Inverse[1]
+        + m[2] * Inverse[2]
+        + m[3] * Inverse[3];
+
+    Inverse /= Determinant;
+
+    float16 InverseT;
+    for(uint i = 0; i < 4; i++) {
+        for(uint j = 0; j < 4; j++) {
+            InverseT[j + i*4] = Inverse[i + j *4];
+        }
+    }
+	return InverseT;
 }
+
+// float16 inverce(float16 m) {
+//     float16 inv;
+//     float det;
+//     int i;
+
+//     inv[0] = m[5]  * m[10] * m[15] - 
+//              m[5]  * m[11] * m[14] - 
+//              m[9]  * m[6]  * m[15] + 
+//              m[9]  * m[7]  * m[14] +
+//              m[13] * m[6]  * m[11] - 
+//              m[13] * m[7]  * m[10];
+
+//     inv[4] = -m[4]  * m[10] * m[15] + 
+//               m[4]  * m[11] * m[14] + 
+//               m[8]  * m[6]  * m[15] - 
+//               m[8]  * m[7]  * m[14] - 
+//               m[12] * m[6]  * m[11] + 
+//               m[12] * m[7]  * m[10];
+
+//     inv[8] = m[4]  * m[9] * m[15] - 
+//              m[4]  * m[11] * m[13] - 
+//              m[8]  * m[5] * m[15] + 
+//              m[8]  * m[7] * m[13] + 
+//              m[12] * m[5] * m[11] - 
+//              m[12] * m[7] * m[9];
+
+//     inv[12] = -m[4]  * m[9] * m[14] + 
+//                m[4]  * m[10] * m[13] +
+//                m[8]  * m[5] * m[14] - 
+//                m[8]  * m[6] * m[13] - 
+//                m[12] * m[5] * m[10] + 
+//                m[12] * m[6] * m[9];
+
+//     inv[1] = -m[1]  * m[10] * m[15] + 
+//               m[1]  * m[11] * m[14] + 
+//               m[9]  * m[2] * m[15] - 
+//               m[9]  * m[3] * m[14] - 
+//               m[13] * m[2] * m[11] + 
+//               m[13] * m[3] * m[10];
+
+//     inv[5] = m[0]  * m[10] * m[15] - 
+//              m[0]  * m[11] * m[14] - 
+//              m[8]  * m[2] * m[15] + 
+//              m[8]  * m[3] * m[14] + 
+//              m[12] * m[2] * m[11] - 
+//              m[12] * m[3] * m[10];
+
+//     inv[9] = -m[0]  * m[9] * m[15] + 
+//               m[0]  * m[11] * m[13] + 
+//               m[8]  * m[1] * m[15] - 
+//               m[8]  * m[3] * m[13] - 
+//               m[12] * m[1] * m[11] + 
+//               m[12] * m[3] * m[9];
+
+//     inv[13] = m[0]  * m[9] * m[14] - 
+//               m[0]  * m[10] * m[13] - 
+//               m[8]  * m[1] * m[14] + 
+//               m[8]  * m[2] * m[13] + 
+//               m[12] * m[1] * m[10] - 
+//               m[12] * m[2] * m[9];
+
+//     inv[2] = m[1]  * m[6] * m[15] - 
+//              m[1]  * m[7] * m[14] - 
+//              m[5]  * m[2] * m[15] + 
+//              m[5]  * m[3] * m[14] + 
+//              m[13] * m[2] * m[7] - 
+//              m[13] * m[3] * m[6];
+
+//     inv[6] = -m[0]  * m[6] * m[15] + 
+//               m[0]  * m[7] * m[14] + 
+//               m[4]  * m[2] * m[15] - 
+//               m[4]  * m[3] * m[14] - 
+//               m[12] * m[2] * m[7] + 
+//               m[12] * m[3] * m[6];
+
+//     inv[10] = m[0]  * m[5] * m[15] - 
+//               m[0]  * m[7] * m[13] - 
+//               m[4]  * m[1] * m[15] + 
+//               m[4]  * m[3] * m[13] + 
+//               m[12] * m[1] * m[7] - 
+//               m[12] * m[3] * m[5];
+
+//     inv[14] = -m[0]  * m[5] * m[14] + 
+//                m[0]  * m[6] * m[13] + 
+//                m[4]  * m[1] * m[14] - 
+//                m[4]  * m[2] * m[13] - 
+//                m[12] * m[1] * m[6] + 
+//                m[12] * m[2] * m[5];
+
+//     inv[3] = -m[1] * m[6] * m[11] + 
+//               m[1] * m[7] * m[10] + 
+//               m[5] * m[2] * m[11] - 
+//               m[5] * m[3] * m[10] - 
+//               m[9] * m[2] * m[7] + 
+//               m[9] * m[3] * m[6];
+
+//     inv[7] = m[0] * m[6] * m[11] - 
+//              m[0] * m[7] * m[10] - 
+//              m[4] * m[2] * m[11] + 
+//              m[4] * m[3] * m[10] + 
+//              m[8] * m[2] * m[7] - 
+//              m[8] * m[3] * m[6];
+
+//     inv[11] = -m[0] * m[5] * m[11] + 
+//                m[0] * m[7] * m[9] + 
+//                m[4] * m[1] * m[11] - 
+//                m[4] * m[3] * m[9] - 
+//                m[8] * m[1] * m[7] + 
+//                m[8] * m[3] * m[5];
+
+//     inv[15] = m[0] * m[5] * m[10] - 
+//               m[0] * m[6] * m[9] - 
+//               m[4] * m[1] * m[10] + 
+//               m[4] * m[2] * m[9] + 
+//               m[8] * m[1] * m[6] - 
+//               m[8] * m[2] * m[5];
+
+//     det = m[0] * inv[0] + m[1] * inv[4] + m[2] * inv[8] + m[3] * inv[12];
+
+//     if (det == 0)
+//         return false;
+
+//     det = 1.0 / det;
+
+//     for (i = 0; i < 16; i++)
+//         inv[i] = inv[i] * det;
+
+//     return true;
+// }
 
 float3 barycentric(float4 A, float4 B, float4 C, float3 P) {
     float3 s[2];
@@ -35,12 +220,6 @@ float3 barycentric(float4 A, float4 B, float4 C, float3 P) {
     return (float3)( -1, 1, 1);
 }
 
-
-void _swap_float4(float4 *a, float4 *b) {
-    float4 tmp = *b;
-    *b = *a;
-    *a = tmp;
-} 
 
 // void triangle(const float4 *pts, global float *zbuffer, global uchar4 *image, uint w, uint h, uchar4 color) {
 //     float4 t0 = pts[0];
@@ -92,7 +271,25 @@ float4 normal(const float4 *pts){
     return res;
 }
 
-void triangle(const float4 *pts, global float *zbuffer, global uchar4 *image, uint w, uint h, uchar4 color) {
+
+float4 applyMatrix(float4 p, float16 mvp) {
+    float4 res = (float4)(
+        p.x * mvp[0] +  p.y * mvp[4] + p.z * mvp[8 ] + p.w * mvp[12],
+        p.x * mvp[1] +  p.y * mvp[5] + p.z * mvp[9 ] + p.w * mvp[13], 
+        p.x * mvp[2] +  p.y * mvp[6] + p.z * mvp[10] + p.w * mvp[14], 
+        p.x * mvp[3] +  p.y * mvp[7] + p.z * mvp[11] + p.w * mvp[15]
+    );
+
+    res.x /= res.w;
+    res.y /= res.w;
+    // res.z /= res.w;
+
+    res.w = 1;
+    
+    return res;
+}
+
+void triangle(const float4 *pts, global float *zbuffer, global float2 *velocity, global uchar4 *image, uint w, uint h, uchar4 color, float16 MVP, float16 MVP_old) {
     float3 LightVec = (float3)(3 / 5.0f, -4 / 5.0f, 0);
 
     float4 n = normal(pts);
@@ -127,6 +324,28 @@ void triangle(const float4 *pts, global float *zbuffer, global uchar4 *image, ui
     //      pts[2].x, pts[2].y, pts[2].z
     //     );
 
+    float16 iMVP = inverce(MVP);
+    // float16 iMVP = MVP;
+
+    // printf("MVP\n%f %f %f %f \n%f %f %f %f \n%f %f %f %f \n%f %f %f %f \n\n"
+    // "iMVP\n%f %f %f %f \n%f %f %f %f \n%f %f %f %f \n%f %f %f %f \n\n", 
+    //     MVP[0], MVP[1], MVP[2], MVP[3],
+    //     MVP[4], MVP[5], MVP[6], MVP[7],
+    //     MVP[8], MVP[9], MVP[10],MVP[11],
+    //     MVP[12],MVP[13],MVP[14],MVP[15], 
+    //     iMVP[0], iMVP[1], iMVP[2], iMVP[3],
+    //     iMVP[4], iMVP[5], iMVP[6], iMVP[7],
+    //     iMVP[8], iMVP[9], iMVP[10], iMVP[11],
+    //     iMVP[12],iMVP[13], iMVP[14], iMVP[15]
+    // );
+
+    // float4 testP = (float4)(5,5,5, 1);
+    // float4 testPA = applyMatrix(testP, MVP);
+    // float4 testPAR = applyMatrix(testPA, iMVP);
+
+
+    // printf("%f %f %f", testP.x, testPA.x, testPAR.x);
+
     float3 P;
     for (P.x = bboxmin.x; P.x <= bboxmax.x; P.x++) {
         for (P.y = bboxmin.y; P.y <= bboxmax.y; P.y++) {
@@ -143,7 +362,7 @@ void triangle(const float4 *pts, global float *zbuffer, global uchar4 *image, ui
             
             // printf("P: %f %f %f, pos: %d, z: %f \n", P.x, P.y, P.z, pos, zbuffer[pos]);
 
-            if (zbuffer[pos] < P.z) {
+            if (zbuffer[pos] < P.z && P.z < 10.f) {
                 // printf("FILL P: %f %f %f, pos: %d, z: %f Color: %u %u %u %u \n", P.x, P.y, P.z, pos, zbuffer[pos],  color.x, color.y, color.z, color.w);
 
                 zbuffer[pos] = P.z;
@@ -152,28 +371,17 @@ void triangle(const float4 *pts, global float *zbuffer, global uchar4 *image, ui
 
                 image[pos] = color;
 
+                float4 nP = (float4)(P, 1);
+
+                float4 oldP = applyMatrix(applyMatrix(nP, iMVP), MVP_old);
+
+                velocity[pos] = (float2)(oldP.x - P.x, oldP.y - P.y);
+
             }
         }
     }
 }
 
-
-float4 applyMatrix(float4 p, float16 mvp) {
-    float4 res = (float4)(
-        p.x * mvp[0] +  p.y * mvp[4] + p.z * mvp[8 ] + p.w * mvp[12],
-        p.x * mvp[1] +  p.y * mvp[5] + p.z * mvp[9 ] + p.w * mvp[13], 
-        p.x * mvp[2] +  p.y * mvp[6] + p.z * mvp[10] + p.w * mvp[14], 
-        p.x * mvp[3] +  p.y * mvp[7] + p.z * mvp[11] + p.w * mvp[15]
-    );
-
-    res.x /= res.w;
-    res.y /= res.w;
-    // res.z /= res.w;
-
-    res.w = 1;
-    
-    return res;
-}
 
 
 // float4 applyMatrix(float4 p, float16 mvp) {
@@ -203,7 +411,8 @@ float4 world2screen(float4 v, uint2 wh) {
 kernel void render(global uchar4 *pixels,
                    global float *zbuffer,
                    global const uint2 *wh,
-                   global const cl_polygon *polygons
+                   global const cl_polygon *polygons,
+                   global float2 *velocity
                    ) {
 
     int i = get_global_id(0);
@@ -246,6 +455,6 @@ kernel void render(global uchar4 *pixels,
     
 
 
-    triangle(pointsApplyed, zbuffer, pixels, wh->x, wh->y, colors[i % 4]);
+    triangle(pointsApplyed, zbuffer, velocity, pixels, wh->x, wh->y, colors[i % 4], P.mvp, P.mvp_old);
 
 }
